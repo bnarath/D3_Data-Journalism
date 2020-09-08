3564bb64fe5ace759c4bea8f24a84b4cac9ff125
Bincy Narath <bincybaburajnarath@gmail.com>
2020-09-08 13:56:59 -0400

    SVG and Group added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,27 @@
-//Step1:- Explore CSV
+//Step1:- Decide SVG and Group Frames
+//This will be changed to window dependent later
+var svgWidth = 960;
+var svgHeight = 600;
+
+var margin = {
+  top: 20,
+  right: 20,
+  bottom: 150,
+  left: 150
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+//Step2:- Append the SVG and chartGroup to the html
+var svg = d3.select("#scatter").append("svg")
+                        .attr("height", svgHeight)
+                        .attr("width", svgWidth);
+
+var chartGroup =  svg.append("g")
+                     .attr("transform", `translate(${margin.left}, ${margin.top})`)
+
+//Step3:- Explore CSV
 d3.csv('assets/data/data.csv').then(function(data, err){
     //Throw err if exists
     if (err) throw err;
@@ -6,6 +29,8 @@ d3.csv('assets/data/data.csv').then(function(data, err){
     console.log(data);
     //abbr, poverty, age, income, obesity, smokes, healthcare are the attributes to be considered
     //data is in the form of array of objects
+
+
 
 }).catch(function(error){
     console.warn(error);
