1484944f4c25f64f0a62078f48e0686bcd03e03b
Bincy Narath <bincybaburajnarath@gmail.com>
2020-09-08 14:19:25 -0400

    scale function

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,21 @@ var svg = d3.select("#scatter").append("svg")
 var chartGroup =  svg.append("g")
                      .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
-//Step3:- Explore CSV
+//Step3:- Scale function
+function scale(data, chosenXAxis, y=true){
+    // Input:- data, chosenXAxis(attribute in the data)
+    // Output:- Linear scale
+    // y: Indicates y axis or not
+    var upper = (y==true) ? svgHeight: svgWidth;
+    var linearScale = d3.scaleLinear()
+                         .domain([d3.min(data, d=>d[chosenXAxis])*0.8, d3.max(data, d=>d[chosenXAxis])*1.2])
+                         .range([0, upper])
+    return linearScale;
+}
+
+
+
+//Step5:- Explore CSV
 d3.csv('assets/data/data.csv').then(function(data, err){
     //Throw err if exists
     if (err) throw err;
@@ -30,7 +44,7 @@ d3.csv('assets/data/data.csv').then(function(data, err){
     //abbr, poverty, age, income, obesity, smokes, healthcare are the attributes to be considered
     //data is in the form of array of objects
 
-
+    //console.log(scale(data, "age", y=false));
 
 }).catch(function(error){
     console.warn(error);
